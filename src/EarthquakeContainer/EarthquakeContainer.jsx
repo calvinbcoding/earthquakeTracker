cad7fc77bbe78f8f34ffa3f383613f5d637e11d7
Calvin Bruch <calvinbcoding@gmail.com>
2019-05-07 16:28:32 -0600

    "everything working with time formatted!"
    Signed-off-by: Calvin Bruch <calvinbcoding@gmail.com>

--- src/EarthquakeContainer/EarthquakeContainer.jsx
+++ src/EarthquakeContainer/EarthquakeContainer.jsx
@@ -5,14 +5,32 @@ class EarthquakeContainer extends Component{
     constructor(){
         super();
         this.state = {
-
-        }
+        
     }
+}
+  
+     timeConverter(UNIX_timestamp){
+        let a = new Date(UNIX_timestamp);
+        let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        let year = a.getFullYear();
+        let month = months[a.getMonth()];
+        let date = a.getDate();
+        let hour = a.getHours();
+        let min = a.getMinutes();
+        let sec = a.getSeconds();
+        let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+        return time;
+      }
+      //console.log(timeConverter(1556103414100));
+    
+    
     render(){
         const earthquakeList = this.props.earthquakes.map((earthquake, i) => {
+            const formattedTime = this.timeConverter(earthquake.properties.time);
+            console.log(formattedTime);
             return (
                 <div id = "info" key = {i}>
-                    <p>M {earthquake.properties.mag} - {earthquake.properties.place} / </p>
+                    <p>M {earthquake.properties.mag} - {earthquake.properties.place} / {formattedTime}</p>
                 </div>
             )
         })
@@ -23,7 +41,6 @@ class EarthquakeContainer extends Component{
         )
     }
 }
-
 
 
 export default EarthquakeContainer;
